70b0b708185ba11397020635a9f22d92db7fe78a
Jomi O <78754002+Jomi-0@users.noreply.github.com>
2024-11-26 15:48:49 +0000

    Q1Length

--- Q1Length.py
+++ Q1Length.py
@@ -1,2 +1,11 @@
 # Create a program that will accept a word from a user and return the length of that word. 
 # Make this program in a loop with option to exit when the use types in quit.
+word=(input("Type a word: "))
+if word.lower() == 'quit':
+        print("Goodbye!")
+if word.isalpha :
+    print("You have",len(word)," letters")
+    
+else:
+    print("Input must be a word")
+
